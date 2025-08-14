45ee7267c1a095f3c13a163a82b248662c4a5cd7
SanYuAung-KBZ <sanyu.aung@kbzbank.com>
2025-08-14 11:43:30 +0630

    first commit

---
+++ NZWalks.API/Controllers/StudendsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudendsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[]
+            {
+                "John Doe",
+                "Jane Smith",
+                "Alice Johnson",
+                "Bob Brown",
+                "Charlie Davis",
+                "Diana Prince",
+                "Ethan Hunt"
+            };
+            return Ok(studentNames);
+        }
+    }
+}
